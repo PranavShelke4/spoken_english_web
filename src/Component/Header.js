72d8d5f1bcf2a071e3d8d08e3f041e9c1a120f14
Pranav <pranavshelke4@gmail.com>
2024-02-04 15:42:15 +0530

    Add new layout to Header component

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -5,7 +5,22 @@ import Container from "react-bootstrap/Container";
 function Header() {
   return (
     <div className="header">
-      <Container>Header</Container>
+      <Container>
+        <div className="header-boxs">
+          <div className="left-box">
+            <h1>Learn to code by watching others</h1>
+            <p>
+              See how experienced developers solve problems in real-time.
+              Watching scripted tutorials is great, but understanding how
+              developers think is invaluable.
+            </p>
+            <div className="button-box">
+              <button>Try it free 7 days</button>
+            </div>
+          </div>
+          <div className="right-box">dassd</div>
+        </div>
+      </Container>
     </div>
   );
 }
