fd11db8109bb8fad01d86dc0235ecfee8ba03ce4
Pranav <pranavshelke4@gmail.com>
2024-02-04 16:16:59 +0530

    Update header design and content

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -8,15 +8,10 @@ function Header() {
       <Container>
         <div className="header-boxs">
           <div className="left-box">
-            <h1>Learn to code by watching others</h1>
-            <p>
-              See how experienced developers solve problems in real-time.
-              Watching scripted tutorials is great, but understanding how
-              developers think is invaluable.
-            </p>
-            <div className="button-box">
-              <button>Try it free 7 days</button>
-            </div>
+            <h1>GROW UP <br /><span>YOUR</span> SKILL<br /> IN MINUTES</h1>
+           
+              <button className="header-button">Try it free 7 days</button>
+            
           </div>
           <div className="right-box">dassd</div>
         </div>
