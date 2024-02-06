6b1ed7e90dc26a8ec46f3c46e7ca7b2611b434b0
Pranav <pranavshelke4@gmail.com>
2024-02-06 23:18:10 +0530

    Fix capitalization of header.css import

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Style/Header.css";
+import "../Style/header.css";
 import Container from "react-bootstrap/Container";
 import img1 from "../assets/img1.png";
 
