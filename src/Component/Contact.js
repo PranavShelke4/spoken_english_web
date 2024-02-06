ab14e3b5428eda6207e8cf9c496bd5c7f050976a
Pranav <pranavshelke4@gmail.com>
2024-02-07 00:08:19 +0530

    Add Contact component and CSS file

---
+++ src/Component/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import "../Style/Contact.css";
+
+function Contact() {
+  return (
+    <div className="Contact">
+      <Container>
+        <h1>CONTACT</h1>
+        <h1>Get In touch</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default Contact;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from '../Component/Header'
 import Courses from '../Component/Courses'
 import AboutUs from '../Component/AboutUs'
+import Contact from '../Component/Contact'
 
 function Home() {
   return (
     <>
         <Header />
         <Courses />       
-        <AboutUs />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+        <AboutUs />       
+        <Contact />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
     </>
   )                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 }
